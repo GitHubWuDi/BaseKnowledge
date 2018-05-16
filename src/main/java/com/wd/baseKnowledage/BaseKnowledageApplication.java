0817a3dc6f9680e96f333c091d8218f9dc8a1c34
wudi <wudi891012@163.com>
2018-05-16 22:17:49 +0800

    完成项目构建工作

---
+++ src/main/java/com/wd/baseKnowledage/BaseKnowledageApplication.java
@@ -0,0 +1,12 @@
+package com.wd.baseKnowledage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseKnowledageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseKnowledageApplication.class, args);
+	}
+}
