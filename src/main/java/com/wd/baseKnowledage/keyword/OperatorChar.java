73396c46cd4e049cf57ebc1697eb492328fd5666
wudi <wudi891012@163.com>
2018-06-04 22:19:39 +0800

    基础代码提交

---
+++ src/main/java/com/wd/baseKnowledage/keyword/OperatorChar.java
@@ -0,0 +1,25 @@
+package com.wd.baseKnowledage.keyword;
+/**
+* @author wudi
+* @version 创建时间：2018年6月4日 下午10:06:40
+* @ClassName 类名称
+* @Description 类描述
+*/
+public class OperatorChar {
+
+	public static void test1(){
+		System.out.println(3*0.1==0.3);
+		System.out.println(3*0.1);
+	}
+	
+	
+	public static void test2(){
+		float f = 3.4f;
+		System.out.println(f);
+	}
+	
+	public static void main(String[] args) {
+	    test1();
+	    test2();
+	}
+}
