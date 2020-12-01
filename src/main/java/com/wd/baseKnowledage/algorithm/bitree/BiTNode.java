5e5dcf41a2a8d2126deb5168072db72faabcb86d
wudi <wudi@vrvmail.com.cn>
2020-12-01 10:04:14 +0800

    二叉树数据结构构造

---
+++ src/main/java/com/wd/baseKnowledage/algorithm/bitree/BiTNode.java
@@ -0,0 +1,17 @@
+package com.wd.baseKnowledage.algorithm.bitree;
+
+import lombok.Data;
+
+/**
+* @author wudi E-mail:wudi891012@163.com
+* @version 创建时间：2020年12月1日 上午9:10:33
+* 类说明：二叉树数据结构
+*/
+@Data
+public class BiTNode {
+
+	private Integer data;
+	private BiTNode lchild;
+	private BiTNode rchild;
+	
+}
