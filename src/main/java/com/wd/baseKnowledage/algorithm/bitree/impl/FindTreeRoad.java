73e7dfc334c94645fd7b54380559600e059f6177
wudi <wudi@vrvmail.com.cn>
2020-12-10 09:16:25 +0800

    添加对应的注释

--- src/main/java/com/wd/baseKnowledage/algorithm/bitree/impl/FindTreeRoad.java
+++ src/main/java/com/wd/baseKnowledage/algorithm/bitree/impl/FindTreeRoad.java
@@ -8,6 +8,8 @@ import com.wd.baseKnowledage.algorithm.bitree.BiTNode;
 * @author wudi E-mail:wudi891012@163.com
 * @version 创建时间：2020年12月10日 上午8:55:25
 * 类说明：如何在二叉树中找出与输入整数相等的所有路径
+* 可以通过对二叉树的遍历找出所有的路径，然后判断各条路径上所有结点的值的和是否与给定的整数相等，如果相等，则打印出这条路径。
+* 对二叉树进行先序遍历，把遍历的路径记录下来，当遍历到叶子结点时，判断当前的路径上所有结点数据的和是否等于给定的整数，如果相等则输出路径信息。
 */
 public class FindTreeRoad {
 
