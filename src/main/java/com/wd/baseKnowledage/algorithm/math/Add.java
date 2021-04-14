8c07cee31d707e9c53d3011f7cc914c42840ffd7
wudi <wudi@vrvmail.com.cn>
2021-04-14 18:50:32 +0800

    如何不用加减乘除运算实现加法运算

---
+++ src/main/java/com/wd/baseKnowledage/algorithm/math/Add.java
@@ -0,0 +1,34 @@
+package com.wd.baseKnowledage.algorithm.math;
+
+/**
+ * 如何不用加减乘除运算实现加法运算
+ * @author Administrator
+ *
+ */
+public class Add {
+
+	
+	/**
+	 * 1）二进制各个位相加而不考虑进位。在不考虑进位的时候加法操作可以用异或操作代替。
+       2）计算进位，由于只有1+1才会产生进位，因此，进位的计算可以用“与”操作代替。进位的计算方法为：先做与运算，再把运算结果左移一位。
+       3）不断对1）、2）两步得到的结果相加，直到进位为0时为止。
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	public static int add(int n1,int n2) {
+		int sum = 0; //保存不进位结果
+		int carry = 0; //保存进位值
+		do {
+			sum=n1^n2; //异或代替不进位相加
+			carry = (n1&n2)<<1; //与操作代理计算进位值
+			n1 = sum;
+			n2 = carry;
+		}while(carry!=0);   //判断进位值是否为0
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(add(1000,200));
+	}
+}
